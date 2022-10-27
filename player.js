f82c71270554b58a184b6cb92af46a34ca8ebfa8
Mykola Masiuk <masiuk.mykola@gmail.com>
2022-10-27 22:21:12 +0300

    add state

--- player.js
+++ player.js
@@ -1,3 +1,5 @@
+import { Sitting, Running } from "./playerStates.js";
+
 export class Player {
   constructor(game) {
     this.game = game;
@@ -8,10 +10,16 @@ export class Player {
     this.vy = 0;
     this.weight = 1;
     this.image = document.getElementById("player");
+    this.frameX = 0;
+    this.frameY = 0;
     this.speed = 0;
     this.maxSpeed = 10;
+    this.states = [new Sitting(this), new Running(this)];
+    this.currentState = this.states[0];
+    this.currentState.enter();
   }
   update(input) {
+    this.currentState.handleInput(input);
     // horizontal movement
     this.x += this.speed;
     if (input.includes("ArrowRight")) this.speed = this.maxSpeed;
@@ -21,7 +29,7 @@ export class Player {
     if (this.x > this.game.width - this.width)
       this.x = this.game.width - this.width;
     // vertical movement
-    if (input.includes("ArrowUp") && this.onGround()) this.vy -= 10;
+    // if (input.includes("ArrowUp") && this.onGround()) this.vy -= 20;
     this.y += this.vy;
     if (!this.onGround()) this.vy += this.weight;
     else this.vy = 0;
@@ -29,8 +37,8 @@ export class Player {
   draw(context) {
     context.drawImage(
       this.image,
-      0,
-      0,
+      this.frameX * this.width,
+      this.frameY * this.height,
       this.width,
       this.height,
       this.x,
@@ -41,5 +49,9 @@ export class Player {
   }
   onGround() {
     return this.y >= this.game.height - this.height;
+  }
+  setState(state) {
+    this.currentState = this.states[state];
+    this.currentState.enter();
   }
 }
